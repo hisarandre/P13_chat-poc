50e9e26f9fcf23d1bcde20da40ecef7795c36207
Sara Andre <hisarandre@gmail.com>
2025-11-22 18:59:45 +0900

    feat: add javadoc

--- back/src/main/java/com/yourcaryourway/back/controller/ChatController.java
+++ back/src/main/java/com/yourcaryourway/back/controller/ChatController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+/**
+ * Handles real time chat between clients and support.
+ * Sends messages via STOMP with WebSocket.
+ */
 @RestController
 public class ChatController {
 
@@ -20,21 +23,42 @@ public class ChatController {
         this.chatService = chatService;
     }
 
+    /**
+     * Gets the chat message history.
+     *
+     * @return a copy of the message list
+     */
     @GetMapping("/api/messages")
     public List<ChatMessage> getMessages() {
         return chatService.getMessages();
     }
 
+    /**
+     * Handles messages from participant.
+     * The message is sent to all chat participants.
+     *
+     * @param chatMessage the message sent
+     */
     @MessageMapping("/chat.send")
     public void sendMessage(@Payload ChatMessage chatMessage) {
         chatService.sendMessage(chatMessage, MessageType.CHAT);
     }
 
+    /**
+     * Notifies that a user has joined the chat.
+     *
+     * @param chatMessage the notification message with user information
+     */
     @MessageMapping("/chat.join")
     public void userJoined(@Payload ChatMessage chatMessage) {
         chatService.sendMessage(chatMessage, MessageType.JOIN);
     }
 
+    /**
+     * Notifies that a user has left the chat.
+     *
+     * @param chatMessage the notification message with user information
+     */
     @MessageMapping("/chat.leave")
     public void userLeft(@Payload ChatMessage chatMessage) {
         chatService.sendMessage(chatMessage, MessageType.LEAVE);
